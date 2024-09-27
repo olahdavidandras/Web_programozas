7d38f50bfd79336912eea24544c090061a5679d6
olahdavidandras <olahtdavidandras@uni.sapientia.ro>
2024-09-27 17:50:29 +0300

    2.

--- HF2/Feladat4.php
+++ HF2/Feladat4.php
@@ -1,7 +1,46 @@
 <?php
 
+function atalakit($tomb, $meret) {
+    $res = array();
+
+    foreach ($tomb as $kulcs => $ertek) {
+        if ($meret == "kisbetus") {
+            $res[$kulcs] = strtolower($ertek);
+        } elseif ($meret == "nagybetus") {
+            $res[$kulcs] = strtoupper($ertek);
+        }
+    }
+
+    return $res;
+}
+
 $szinek = array('A' => 'Kek', 'B' => 'Zold', 'c' => 'Piros');
 
+$kisbetus = atalakit($szinek, "kisbetus");
+print_r($kisbetus);
 
+echo "<br>";
+
+$nagybetus = atalakit($szinek, "nagybetus");
+print_r($nagybetus);
+
+echo "<br>";
+
+function atalakitMap($tomb, $meret) {
+    if ($meret == "kisbetus") {
+        return array_map('strtolower', $tomb);
+    } elseif ($meret == "nagybetus") {
+        return array_map('strtoupper', $tomb);
+    }
+    return $tomb;
+}
+
+$kisbetusMap = atalakitMap($szinek, "kisbetus");
+print_r($kisbetusMap);
+
+echo "<br>";
+
+$nagybetusMap = atalakitMap($szinek, "nagybetus");
+print_r($nagybetusMap);
 
 ?>
