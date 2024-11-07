c784b2470690b1b88a1500164b5a30e516f5b364
olahdavidandras <olahtdavidandras@uni.sapientia.ro>
2024-11-07 17:12:32 +0200

    Final2

--- HF7/bonusz.php
+++ HF7/bonusz.php
@@ -7,138 +7,110 @@ if (!isset($_SESSION['tasks'])) {
 
 // 1. feladat
 $lista = array(5, '5', '05', 12.3, '16.7', 'five', 'true', 0xDECAFBAD, '10e200');
-ob_start();
+$task1 = "";
 foreach ($lista as $elem) {
     $valtozo = gettype($elem);
-    echo $valtozo;
+    $task1 .= $valtozo;
     if (is_numeric($elem)) {
-        echo " Igen" . "<br>";
+        $task1 .= " Igen<br>";
     } else {
-        echo " Nem" . "<br>";
+        $task1 .= " Nem<br>";
     }
 }
-$_SESSION['tasks']['task1'] = ob_get_clean();
+$_SESSION['tasks']['task1'] = $task1;
 
 // 2. feladat
 $sec = 21.1;
-ob_start();
 if (is_int($sec)) {
     $orak = floor($sec / 3600);
     $maradekSec = $sec % 3600;
     $percek = floor($maradekSec / 60);
     $masodperc = $maradekSec % 60;
 
-    echo "<p><strong>{$orak} ora</strong>, {$percek} perc es {$masodperc} masodperc.</p>";
+    $_SESSION['tasks']['task2'] = "<p>{$orak} ora, {$percek} perc es {$masodperc} masodperc.</p>";
 } else {
-    echo "Nem egesz szam volt megadva";
+    $_SESSION['tasks']['task2'] = "Nem egesz szam volt megadva";
 }
-$_SESSION['tasks']['task2'] = ob_get_clean();
 
 // 3. feladat
 $szam1 = 12;
 $szam2 = 2.1;
-ob_start();
-echo "$szam1 + $szam2 = " . ($szam1 + $szam2) . "<br>";
-echo "$szam1 - $szam2 = " . ($szam1 - $szam2) . "<br>";
-echo "$szam1 * $szam2 = " . ($szam1 * $szam2) . "<br>";
-echo "$szam1 / $szam2 = " . ($szam1 / $szam2) . "<br>";
-echo "$szam1 ^ $szam2 = " . (pow($szam1, $szam2)) . "<br>";
-$_SESSION['tasks']['task3'] = ob_get_clean();
+$task3 = "";
+$task3 .= "$szam1 + $szam2 = " . ($szam1 + $szam2) . "<br>";
+$task3 .= "$szam1 - $szam2 = " . ($szam1 - $szam2) . "<br>";
+$task3 .= "$szam1 * $szam2 = " . ($szam1 * $szam2) . "<br>";
+$task3 .= "$szam1 / $szam2 = " . ($szam1 / $szam2) . "<br>";
+$task3 .= "$szam1 ^ $szam2 = " . (pow($szam1, $szam2)) . "<br>";
+$_SESSION['tasks']['task3'] = $task3;
 
 // 4. feladat
-ob_start();
-echo <<<EOD
-<table border="1" border-spacing="0" padding="0">
-EOD;
-
+$task4 = "<table border=\"1\" border-spacing=\"0\" padding=\"0\">";
 for ($sor = 1; $sor <= 3; $sor++) {
-    echo <<<EOD
-    <tr>
-EOD;
+    $task4 .= "<tr>";
     for ($oszlop = 1; $oszlop <= 3; $oszlop++) {
         $ossz = $sor + $oszlop;
-        if ($ossz % 2 == 0) {
-            echo <<<EOD
-            <td height="35px" width="30px" bgcolor="#FFFFFF"></td>
-EOD;
-        } else {
-            echo <<<EOD
-            <td height="35px" width="30px" bgcolor="#000000"></td>
-EOD;
-        }
+        $bgcolor = ($ossz % 2 == 0) ? "#FFFFFF" : "#000000";
+        $task4 .= "<td height=\"35px\" width=\"30px\" bgcolor=\"$bgcolor\"></td>";
     }
-
-    echo <<<EOD
-    </tr>
-EOD;
+    $task4 .= "</tr>";
 }
-
-echo <<<EOD
-</table>
-EOD;
-$_SESSION['tasks']['task4'] = ob_get_clean();
+$task4 .= "</table>";
+$_SESSION['tasks']['task4'] = $task4;
 
 // 5. feladat
 $szam1 = 12;
 $szam2 = 254.12;
 $jel = '*';
-ob_start();
 switch ($jel) {
     case '-':
-        echo $szam1 - $szam2;
+        $_SESSION['tasks']['task5'] = $szam1 - $szam2;
         break;
     case '+':
-        echo $szam1 + $szam2;
+        $_SESSION['tasks']['task5'] = $szam1 + $szam2;
         break;
     case '*':
-        echo $szam1 * $szam2;
+        $_SESSION['tasks']['task5'] = $szam1 * $szam2;
         break;
     case '/':
-        if ($szam2 != 0) {
-            echo $szam1 / $szam2;
-        } else {
-            echo "Nullaval valo osztas!";
-        }
+        $_SESSION['tasks']['task5'] = ($szam2 != 0) ? $szam1 / $szam2 : "Nullaval valo osztas!";
         break;
     default:
-        echo "Nem megfelelő bemenet!";
+        $_SESSION['tasks']['task5'] = "Nem megfelelő bemenet!";
 }
-$_SESSION['tasks']['task5'] = ob_get_clean();
 
 // 6. feladat
 $honap = "4";
-ob_start();
 if ($honap == 1 || $honap == 2 || $honap == 12) {
-    echo "Tel";
+    $task6 = "Tel";
 } elseif ($honap >= 3 && $honap <= 5) {
-    echo "Tavasz";
+    $task6 = "Tavasz";
 } elseif ($honap >= 6 && $honap <= 8) {
-    echo "Nyar";
+    $task6 = "Nyar";
 } elseif ($honap >= 9 && $honap <= 11) {
-    echo "Osz";
+    $task6 = "Osz";
 } else {
-    echo "Nem letezo honapot adtal meg!";
+    $task6 = "Nem letezo honapot adtal meg!";
 }
 
-echo "<br>";
+$task6 .= "<br>";
 
 switch (true) {
     case ($honap == 1 || $honap == 2 || $honap == 12):
-        echo "Tel";
+        $task6 .= "Tel";
         break;
     case ($honap >= 3 && $honap <= 5):
-        echo "Tavasz";
+        $task6 .= "Tavasz";
         break;
     case ($honap >= 6 && $honap <= 8):
-        echo "Nyar";
+        $task6 .= "Nyar";
         break;
     case ($honap >= 9 && $honap <= 11):
-        echo "Osz";
+        $task6 .= "Osz";
         break;
     default:
-        echo "Nem letezo honapot adtal meg!";
+        $task6 .= "Nem letezo honapot adtal meg!";
 }
-$_SESSION['tasks']['task6'] = ob_get_clean();
+$_SESSION['tasks']['task6'] = $task6;
 ?>
 
 <!DOCTYPE html>
